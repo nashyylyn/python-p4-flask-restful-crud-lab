e9787d08bc9dcdf77d6b8b266d186963a01da683
nashylyn <natasha.kamau@student.moringaschool,com>
2025-06-28 22:51:48 +0300

    all

--- server/app.py
+++ server/app.py
@@ -16,6 +16,9 @@ db.init_app(app)
 
 api = Api(app)
 
+# -------------------
+# /plants (GET, POST)
+# -------------------
 
 class Plants(Resource):
 
@@ -38,16 +41,43 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
-
+# -------------------------------
+# /plants/<id> (GET, PATCH, DELETE)
+# -------------------------------
 
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        data = request.get_json()
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+            db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
+# Add resources to API
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
